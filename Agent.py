1696c1019bd4e5009d8881b3a608b68f634eeada
None <None>
2024-05-10 04:44:15 -0700

    Full way comunication

--- Agent.py
+++ Agent.py
@@ -4,83 +4,150 @@ import socket
 import json
 import messages_pb2
 import uuid
+import sys
 
 HOST = "10.0.1.1"
 PORT = 65432
 
+MRN_OWN = "someMrn"
 
-def DiscoverEdgeRouters():
-    # Shall return list of Mrn of all mms edge routers
-    pass
+class Agent:
+    def __init__(self):
+        pass
 
-def ConnectAnonymously(MRN_edge_router):
-    # Connects to a edgerouter vis MRN
-    pass
+    def DiscoverEdgeRouters():
+        # Shall return list of Mrn of all mms edge routers
+        pass
 
-def ReconnectAnonymously(MRN_edge_router, reconnection_token):
-    # arguments ?
-    pass
+    def ConnectAnonymously(MRN_edge_router):
+        # Connects to a edgerouter vis MRN
+        pass
 
-def ConnectAuthenticated(MRN_edge_router, certificate):
-    message = messages_pb2.MmtpMessage()
-    message.msgType = messages_pb2.PROTOCOL_MESSAGE
-    message.uuid = str(uuid.uuid4())
+    def ReconnectAnonymously(MRN_edge_router, reconnection_token):
+        # arguments ?
+        pass
 
-    message.protocolMessage.protocolMsgType = messages_pb2.ProtocolMessageType.CONNECT_MESSAGE
+    def ConnectAuthenticated(self, MRN_edge_router, certificate):
+        message = messages_pb2.MmtpMessage()
+        message.msgType = messages_pb2.PROTOCOL_MESSAGE
+        message.uuid = str(uuid.uuid4())
+        message.protocolMessage.protocolMsgType = messages_pb2.ProtocolMessageType.CONNECT_MESSAGE
+        message.protocolMessage.connectMessage.ownMrn = MRN_edge_router
 
-    #protocolMessage.connectMessage = messages_pb2.Connect()
-    message.protocolMessage.connectMessage.ownMrn = MRN_edge_router
+        message_str = message.SerializeToString()
 
-    message_str = message.SerializeToString()
+        self.EdgeRouter = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.EdgeRouter.connect((HOST, PORT))
+        self.EdgeRouter.sendall(message_str)
+        data = self.EdgeRouter.recv(1024)
 
-    print(message_str)
-    print(b"Hello, world")
+        message = messages_pb2.MmtpMessage()
+        message.ParseFromString(data)
+        #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        #    s.connect((HOST, PORT))
+        #    s.sendall(message_str)
+        #    data = s.recv(1024)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(message_str)
-        data = s.recv(1024)
+        print(data)
+        pass
 
-    pass
+    def ReconnectAuthenticated(MRN_edge_router, reconnection_token):
+        pass
 
-def ReconnectAuthenticated(MRN_edge_router, reconnection_token):
-    pass
+    def Status():
+        pass
 
-def Status():
-    pass
+    def Query():
+        pass
 
-def Query():
-    pass
+    def Subscribe(subject):
+        pass
 
-def Subscribe(subject):
-    pass
+    def Unsubscribe(subject):
+        pass
 
-def Unsubscribe(subject):
-    pass
+    def Subscribe():
+        pass
 
-def Subscribe():
-    pass
+    def Unsubscribe():
+        pass
 
-def Unsubscribe():
-    pass
+    def Send(self, TTL, MRN, message):
+        mmtpMessage = messages_pb2.MmtpMessage()
 
-def Send(TTL, MRN, message):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(b"Hello, world")
-        data = s.recv(1024)
-    pass
+        mmtpMessage.msgType = messages_pb2.PROTOCOL_MESSAGE
+        mmtpMessage.uuid = str(uuid.uuid4())
+        mmtpMessage.protocolMessage.protocolMsgType = messages_pb2.ProtocolMessageType.SEND_MESSAGE
 
-def Notify():
-    pass
+        applicationMessage = mmtpMessage.protocolMessage.sendMessage.applicationMessage
+        applicationMessage.body = message.encode('utf-8')
+        applicationMessage.signature = "someSig"
 
-def Receive(filter):
-    pass
+        applicationMessageHeader = applicationMessage.header
 
-def Disconnect():
-    pass
+        applicationMessageHeader.recipients.recipients.extend([MRN])
+        applicationMessageHeader.expires = TTL
+        applicationMessageHeader.sender = MRN_OWN
+        applicationMessageHeader.bodySizeNumBytes = len(applicationMessage.body)
 
-ConnectAuthenticated("someMrn", "someCertificate")
+        message_str = mmtpMessage.SerializeToString()
+
+        self.EdgeRouter.sendall(message_str)
+    #    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            #s.connect((HOST, PORT))
+            #s.sendall(message_str)
+
+        data = self.EdgeRouter.recv(1024)
+
+        print(data)
+        pass
+
+    def Notify():
+        pass
+
+    def Receive(self, filter=""):
+        mmtpMessage = messages_pb2.MmtpMessage()
+        mmtpMessage.msgType = messages_pb2.PROTOCOL_MESSAGE
+        mmtpMessage.uuid = str(uuid.uuid4())
+        mmtpMessage.protocolMessage.protocolMsgType = messages_pb2.ProtocolMessageType.RECIEVE_MESSAGE
+        message_str = mmtpMessage.SerializeToString()
+
+        self.EdgeRouter.sendall(message_str)
+
+        data = self.EdgeRouter.recv(1024)
+        messageRecv = messages_pb2.MmtpMessage()
+        messageRecv.ParseFromString(data)
+        print(messageRecv.prsponseMessage.applicationMessage[0].body.decode('utf-8'))
+        print(messageRecv.prsponseMessage.response)
+        pass
+
+    def Disconnect():
+        pass
+
+if __name__ == "__main__":
+    if(len(sys.argv) > 1):
+        MRN_OWN = sys.argv[1]
+
+    agent = Agent()
+
+    agent.ConnectAuthenticated(MRN_OWN, "someCertificate")
+
+    isRunning = True;
+    while (isRunning):
+        command = input('Enter command :\n')
+        if (command == "-s"):
+            content = input('Enter message :\n')
+            MRN = input('Enter MRN of reciver :\n')
+            agent.Send(100000, MRN, content)
+        elif (command == "-r"):
+            print("reciving messages")
+            agent.Receive()
+        elif (command == "exit"):
+            print("shutting down")
+            isRunning = False
+        else:
+            print("unknown command")
+
 
 #with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 #    s.connect((HOST, PORT))
--- EdgeRouter.py
+++ EdgeRouter.py
@@ -2,13 +2,49 @@
 
 import socket
 import json
+import uuid
 import messages_pb2
+import threading
 
 HOST = "10.0.1.1"
 PORT = 65432
 
 connections = dict()
 subscriptions = dict()
+
+messageBuffer = dict()
+
+def ConnectionToAgent(conn, addr):
+    mrn = ""
+    with conn:
+        (ip_addr, port) = addr
+        print("Connected by " + str(ip_addr) + ", " + str(port))
+        while True:
+            data = conn.recv(1024)
+            if data:
+                message = messages_pb2.MmtpMessage()
+                message.ParseFromString(data)
+
+                if(message.msgType == messages_pb2.MsgType.PROTOCOL_MESSAGE):
+                    print("ProtocolMessage")
+                    if (message.protocolMessage.protocolMsgType == messages_pb2.ProtocolMessageType.CONNECT_MESSAGE):
+                        mrn = message.protocolMessage.connectMessage.ownMrn
+                        print("    connectMessage")
+                        print(mrn)
+                        print(message.protocolMessage.connectMessage.reconnectToken)
+                        AddConnection(message, ip_addr, port)
+                    elif (message.protocolMessage.protocolMsgType == messages_pb2.ProtocolMessageType.SEND_MESSAGE):
+                        print("    Send Message")
+                        ReceivedSendMessage(message)
+                    elif (message.protocolMessage.protocolMsgType == messages_pb2.ProtocolMessageType.RECIEVE_MESSAGE):
+                        print("    recieve Message")
+                        print(message.protocolMessage.connectMessage.ownMrn)
+                        print(message.protocolMessage.connectMessage.reconnectToken)
+                        HandelReceiveMessage(message, mrn, conn)
+
+                conn.sendall(data)
+    pass
+
 
 def StartUp():
     pass
@@ -23,27 +59,65 @@ def Notify_to(To_Agent):
     pass
 
 
-def Listen():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            (ip_addr, port) = addr
-            print("Connected by " + str(ip_addr) + ", " + str(port))
-            while True:
-                data = conn.recv(1024)
-                message = messages_pb2.MmtpMessage()
-                message.ParseFromString(data)
+def AddConnection(message, ip, port):
+    mrn = message.protocolMessage.connectMessage.ownMrn
+    connections[mrn] = (ip, port)
 
-                if(message.msgType == messages_pb2.MsgType.PROTOCOL_MESSAGE):
-                    print("ProtocolMessage")
-                    if (message.protocolMessage.protocolMsgType == messages_pb2.ProtocolMessageType.CONNECT_MESSAGE):
-                        print("    connectMessage")
+    print(connections[mrn])
+    pass
 
-                if not data:
-                    break
-                conn.sendall(data)
+def ReceivedSendMessage(message):
+    applicationMessage = message.protocolMessage.sendMessage.applicationMessage
+    body = applicationMessage.body.decode('utf-8')
+    length = applicationMessage.header.bodySizeNumBytes
+    recipients = applicationMessage.header.recipients.recipients
+    print(body)
+    print("length = " + str(length))
 
-while True:
-    Listen()
+    for recipient in recipients:
+        print(recipient)
+        if (not recipient in messageBuffer):
+            messageBuffer[recipient] = []
+        messageBuffer[recipient].append(message)
+
+    pass
+
+def HandelReceiveMessage(message, mrn, conn):
+    receiveMessage = message.protocolMessage.receiveMessage
+
+    messagesBuffer = messageBuffer[mrn]
+
+    response = messages_pb2.MmtpMessage()
+    response.msgType = messages_pb2.REDPONSE_MESSAGE
+    response.uuid = str(uuid.uuid4())
+
+    responseMessage = response.prsponseMessage
+    responseMessage.responseToUuid = message.uuid
+    responseMessage.response = messages_pb2.ResponseEnum.GOOD
+
+    for m in messagesBuffer:
+        print(m)
+        applicationMessage = m.protocolMessage.sendMessage.applicationMessage
+        responseMessage.applicationMessage.extend([applicationMessage])
+
+    print(response)
+
+    message_str = response.SerializeToString()
+
+    print(message_str)
+
+    conn.sendall(message_str)
+
+    pass
+
+
+def Listen(s):
+    print("listening at: " + HOST + ", " + str(PORT))
+    s.listen()
+    conn, addr = s.accept()
+    threading.Thread(target=ConnectionToAgent, args=(conn, addr)).start()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    while True:
+        Listen(s)
